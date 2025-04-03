fc856b7b6b7966f179033733ebc7c9c2344b9ca7
dhanajit96 <ajitdhanje@gmail.com>
2025-04-03 11:23:08 +0530

    added add method along with console

---
+++ lib/string_calculator.rb
@@ -0,0 +1,37 @@
+# lib/string_calculator.rb
+class StringCalculator
+  def self.add(numbers)
+    return 0 if numbers.empty?
+
+    delimiter = ','
+    if numbers.start_with?('//')
+      parts = numbers.split("\n", 2)
+      delimiter = parts.first[2..-1]
+      numbers = parts.last
+    end
+
+    nums = numbers.split(/[\n,#{Regexp.escape(delimiter)}]/).map(&:to_i)
+
+    negatives = nums.select { |n| n < 0 }
+    raise "negative numbers not allowed #{negatives.join(',')}" if negatives.any?
+
+    nums.sum
+  end
+end
+
+# console application
+if __FILE__ == $0
+  puts 'Enter numbers to add (press double Enter to exit):'
+  input_lines = []
+  while (line = gets.chomp) != ''
+    input_lines << line
+  end
+  input = input_lines.join("\n")
+
+  begin
+    result = StringCalculator.add(input)
+    puts "Result: #{result}"
+  rescue StandardError => e
+    puts "Error: #{e.message}"
+  end
+end
