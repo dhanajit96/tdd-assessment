20ff92bc61af2d6ecda076b2974e4b171677a765
dhanajit96 <ajitdhanje@gmail.com>
2025-04-03 11:20:28 +0530

    added the 7 test cases as need in spec file

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,35 @@
+# spec/string_calculator_spec.rb
+require 'rspec'
+require_relative '../lib/string_calculator'
+
+describe StringCalculator do
+  describe '.add' do
+    it 'returns 0 for an empty string' do
+      expect(StringCalculator.add('')).to eq(0)
+    end
+
+    it 'returns the number itself for a single number' do
+      expect(StringCalculator.add('1')).to eq(1)
+    end
+
+    it 'returns the sum of two numbers' do
+      expect(StringCalculator.add('1,5')).to eq(6)
+    end
+
+    it 'handles any amount of numbers' do
+      expect(StringCalculator.add('1,2,3,4')).to eq(10)
+    end
+
+    it 'handles new lines as delimiters' do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
+
+    it 'supports custom delimiters' do
+      expect(StringCalculator.add("//&\n1&2\n3&5")).to eq(11)
+    end
+
+    it 'raises an exception for negative numbers' do
+      expect { StringCalculator.add('1,-2,3,-4') }.to raise_error('negative numbers not allowed -2,-4')
+    end
+  end
+end
